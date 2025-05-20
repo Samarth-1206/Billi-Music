c67748f1d56b6fe74ca111d6ecf95985f338edee
Samarth-1206 <pawar06samarth14@gmail.com>
2025-05-20 22:15:19 +0530

    Update must_join.py

--- RessoMusic/plugins/bot/must_join.py
+++ RessoMusic/plugins/bot/must_join.py
@@ -4,9 +4,9 @@ from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForb
 from RessoMusic import app
 
 #--------------------------
-
 MUST_JOIN = "I_kaisen"
-#------------------------
+#--------------------------
+
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
     if not MUST_JOIN:
@@ -22,15 +22,21 @@ async def must_join_channel(app: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://files.catbox.moe/3tfhrd.jpg", caption=f"๏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [๏ sᴜᴘᴘᴏʀᴛ ๏]({link}) ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴄʜᴇᴀᴋ ᴍʏ ғᴇᴀᴛᴜʀᴇs.\n\nᴀғᴛᴇʀ ᴊᴏɪɴ ᴛʜᴇ [๏ ᴄʜᴀɴɴᴇʟ ๏]({link}) ᴄᴏᴍᴇ ʙᴀᴄᴋ ᴛᴏ ᴛʜᴇ ʙᴏᴛ ᴀɴᴅ ᴛʏᴘᴇ /start ᴀɢᴀɪɴ !! ",
+                    photo="https://files.catbox.moe/3tfhrd.jpg",
+                    caption=(
+                        f"<b>๏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ <a href=\"{link}\">๏ sᴜᴘᴘᴏʀᴛ ๏</a> "
+                        f"ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ғᴇᴀᴛᴜʀᴇs.</b>\n\n"
+                        f"<b>ᴀғᴛᴇʀ ᴊᴏɪɴɪɴɢ ᴛʜᴇ <a href=\"{link}\">๏ ᴄʜᴀɴɴᴇʟ ๏</a> "
+                        f"ᴄᴏᴍᴇ ʙᴀᴄᴋ ᴛᴏ ᴛʜᴇ ʙᴏᴛ ᴀɴᴅ ᴛʏᴘᴇ /start ᴀɢᴀɪɴ !!</b>"
+                    ),
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
                                 InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
-                               # InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/i_kaisen"),
                             ]
                         ]
-                    )
+                    ),
+                    parse_mode="html"
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
