ab080521754f559751f4583f7ba01e50cb2774ec
Samarth-1206 <pawar06samarth14@gmail.com>
2025-06-29 14:11:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/GHOULS_NETWORK")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/SN8SUPPORT")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/GHOULS_SUPPORT")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
@@ -81,7 +81,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/3tfhrd.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/tewqls.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/bwtbji.jpg"
